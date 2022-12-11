9bc50707afc9ed235b1577c092b4e436031e4e3a
Ch3ny <ouyi.cheny@gmail.com>
2022-12-11 17:08:51 +0100

    1

--- res/js/main.js
+++ res/js/main.js
@@ -1,16 +1,23 @@
 const play = document.getElementById("play");
 const menu = document.getElementById("menu");
-const preference = document.getElementById("preference")
-const back = document.getElementById("back")
-
+const preference = document.getElementById("preference");
+const back = document.getElementById("back");
+const man = document.getElementById("man");
+const name = document.getElementById("player-name")
 
 play.onclick = () => {
-    menu.style.display="none"
-    preference.style.display="block"
+    menu.style.display="none";
+    preference.style.display="block";
 }
 
 
 back.onclick = () => {
-    menu.style.display="block"
-    preference.style.display="none"
+    menu.style.display="block";
+    preference.style.display="none";
+}
+
+
+man.onclick = () => {
+    preference.style.display="none";
+    name.style.display="block";
 }
